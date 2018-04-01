10d1ecb7f7b419406caf8eb876fef2e84458a892
James Robinson <jrobind@outlook.com>
2018-04-01 13:19:01 +0100

    add basic express and passport config, plus catch-all route

--- server.js
+++ server.js
@@ -7,3 +7,29 @@ const webpackConfig = require('./webpack.config.js');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+app.use(session({
+    secret: 'fit-finder is the best',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// config for passport
+require('./config/passport')(passport);
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//routes
+
+// catch all route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('server has started'));
