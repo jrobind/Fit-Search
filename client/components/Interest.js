5e1e648c2518f80b63d13eb88db685ea1bbe809f
James Robinson <jrobind@outlook.com>
2018-04-06 12:39:00 +0100

    move handleRemoveInterest() logic to InterestContainer

--- client/components/Interest.js
+++ client/components/Interest.js
@@ -1,49 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { apiRemoveInterestRequest } from '../utils/api';
-import { handleGetInterestRequests } from '../actions/interestRequests';
 
-class Interest extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.handleRemoveInterest = this.handleRemoveInterest.bind(this);
-    }
-    
-    handleRemoveInterest(id) {
-        apiRemoveInterestRequest(id)
-            .then((data) => this.props.dispatch(handleGetInterestRequests(this.props.trainerId)))
-            .catch((error) => console.log(error));
-    }
-    
-    render() {
-        const { requests } = this.props;
-        return (
-            <div>
-                {!requests.length ? <p>No requests found...</p> :
-                    <div className="interest-container">  
-                        {requests.map(({ requestee, _id }) => (
-                            <div key={_id} className="interest-card">
-                                <div>User email: {requestee.email}</div> 
-                                <div>User name: {requestee.profile.name}</div> 
-                                <img src={requestee.profile.avatar}/>
-                                <div>User bio: {requestee.profile.bio}</div>
-                                <div>Already contacted the user?</div>
-                                <button onClick={() => {
-                                    this.handleRemoveInterest(_id);
-                                }}>Remove request</button>
-                            </div>
-                        ))}
+const Interest = ({ requests, trainerId, handleRemoveInterest }) => (
+    <div>
+        {!requests.length ? <p>No requests found...</p> :
+            <div className="interest-container">  
+                {requests.map(({ requestee, _id }) => (
+                    <div key={_id} className="interest-card">
+                        <div>User email: {requestee.email}</div> 
+                        <div>User name: {requestee.profile.name}</div> 
+                        <img src={requestee.profile.avatar}/>
+                        <div>User bio: {requestee.profile.bio}</div>
+                        <div>Already contacted the user?</div>
+                        <button onClick={() => {
+                            handleRemoveInterest(_id);
+                        }}>Remove request</button>
                     </div>
-                }
-            </div>   
-        )
-    }
-}
+                ))}
+            </div>
+        }
+    </div>   
+)
 
 Interest.propTypes = {
     requests: PropTypes.array.isRequired,
-    trainerId: PropTypes.string.isRequired
+    trainerId: PropTypes.string.isRequired,
+    handleRemoveInterest: PropTypes.func.isRequired
 }
 
 export default Interest;
--- client/containers/InterestContainer.js
+++ client/containers/InterestContainer.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { apiRemoveInterestRequest } from '../utils/api';
+import { handleGetInterestRequests } from '../actions/interestRequests';
 import Interest from '../components/Interest';
 
-const InterestContainer = (props) => <Interest {...props} />
+class InterestContainer extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.handleRemoveInterest = this.handleRemoveInterest.bind(this);
+    }
+    
+    handleRemoveInterest(requestId) {
+        apiRemoveInterestRequest(requestId)
+            .then((data) => this.props.dispatch(handleGetInterestRequests(this.props.trainerId)))
+            .catch((error) => console.log(error));
+    }
+    
+    render() {
+        return <Interest {...this.props} handleRemoveInterest={this.handleRemoveInterest} />;
+    }
+}
 
 const mapStateToProps = (state) => {
     const { requests } = state.interestRequests;
