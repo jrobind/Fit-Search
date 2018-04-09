69532b40ac44c050b802c67dd1f7805069bf8ca6
James Robinson <jrobind@outlook.com>
2018-04-09 16:13:20 +0100

    add logo to Nav and update Home styles

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import '../styles/components/home.css';
 
 class Home extends Component {
@@ -13,11 +14,28 @@ class Home extends Component {
 
         return(
                 <div className="home-container">
-                    {!loggedIn && !profile ? <p className="welcome-message">Please sign in!</p> : <p className="welcome-message">{name}</p>}
-                    <div className="title-container">
-                        <h1>Fit-Search</h1>
-                        <p>The Hassle Free Personal Trainer Search</p>
+                    <div className="hero">
+                        {!loggedIn && !profile ? null : <p className="welcome-message">{name}</p>}
+                        <div className="title-container">
+                            <h1>FIND YOUR FITNESS GURU</h1>
+                            <p>The hassle-free personal trainer search</p>
+                        </div>
+                        <div className="scroll-container">
+                            <div className="scroll-circle">&#8595;</div>
+                        </div>
                     </div>
+            
+                    <section className="process">
+                        <h1>Finding your perfect personal trainer couldn't be easier.</h1>
+                        <p className="process-explained">
+                            Search our huge selection of user verified personal trainers from across England. Simply sign up and create your own unique user profile, then use our sofisticated search functionality to find the perfect personal trainer for you. Once you find a trainer you like, register your interest. 
+                        </p>
+                        <p className="process-explained">
+                            Trainers will contact you by email to organise a <strong>FREE</strong> taster session. All thats left, is for you to get in the best shape of your life. 
+                        </p>
+                        <Link className="home-sign-up" to='/sign-up'>Sign me up now!</Link>
+
+                    </section>
                 </div>
             )   
     }
--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../styles/components/nav.css';
 
 const Nav = ({ 
@@ -11,21 +11,23 @@ const Nav = ({
     userAuth: { userType },
     userProfile
 }) => (
-    <ul className="navbar">
-        <li><NavLink activeClassName="active" exact to='/'>Fit Finder</NavLink></li>
-    
-        {loggedIn && userType === 'client' || loggedIn && interestRequestSuccess && userType === 'trainer' ? <li><NavLink activeClassName="active" to='/portal'>Profile Portal</NavLink></li> : null}
-    
-        {loggedIn && userType === 'client' && userProfile.profile && <li><NavLink activeClassName="active" to='/search'>Search</NavLink></li>}
-    
-        {interestRequestSuccess && loggedIn && <li><NavLink activeClassName="active" to='/interest'>Interest Requests<span className="request-notifications">{requests.length}</span></NavLink></li>}
-    
-        {!loggedIn ? <li><NavLink activeClassName="active" to='/sign-up'>Sign up!</NavLink></li> : null}
-    
-        {!loggedIn ? <li><NavLink activeClassName="active" to='/login'>login</NavLink></li> : <li><a onClick={() => {
-            handleLogout();
-        }}>logout</a></li>}
-    </ul>
+    <nav>
+        <ul className="navbar">
+            <li className="logo"><Link to='/'><img src={require('../images/logo.png')}/></Link></li>
+
+            {loggedIn && userType === 'client' || loggedIn && interestRequestSuccess && userType === 'trainer' ? <li><NavLink activeClassName="active" to='/portal'>Profile Portal</NavLink></li> : null}
+
+            {loggedIn && userType === 'client' && userProfile.profile && <li><NavLink activeClassName="active" to='/search'>Search</NavLink></li>}
+
+            {interestRequestSuccess && loggedIn && <li><NavLink activeClassName="active" to='/interest'>Interest Requests<span className="request-notifications">{requests.length}</span></NavLink></li>}
+
+            {!loggedIn ? <li><NavLink activeClassName="active" to='/sign-up'>Sign up</NavLink></li> : null}
+
+            {!loggedIn ? <li><NavLink activeClassName="active" to='/login'>login</NavLink></li> : <li><a onClick={() => {
+                handleLogout();
+            }}>logout</a></li>}
+        </ul>
+    </nav>
 );
 
 Nav.propTypes = {
