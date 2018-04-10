50edb1309a6abf781a0b2d85a550b0ebd426578c
James Robinson <jrobind@outlook.com>
2018-04-10 17:55:39 +0100

    add Profile component styles

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from '../styles/components/profile.css';
 
 const Profile = ({ 
     profile: { 
@@ -15,19 +16,27 @@ const Profile = ({
     },
     locationState
 }) => (
-    <div className="portal-container">
-    {locationState && locationState.profileUpdated ? <div className="profile-update">Profile updated!</div> : null}
-        <ul>
-            <li className="profile-title">{name}</li>
-            <li><img src={avatar}/></li>
-            <li className="justify">{bio}</li>
-            {rate ? <li>Hourly rate: £{rate}</li> : null}
-            {region ? <li className="justify bold-me">Region: {region}</li> : null}
-            {base ? <li className="justify bold-me">Based out of: {base}</li> : null}
-            {radius ? <li className="justify bold-me">Radius Covered: {radius}</li> : null}
-            {notes ? <li className="justify bold-me">areas covered notes: {notes}</li> : null}
-        </ul>
-        <Link className="update" to='/portal/update'>Update profile</Link>
+    <div className={styles.profileContainer}>
+    {locationState && locationState.profileUpdated ? <div className={styles.updateSuccess}>Profile updated!</div> : null}
+    <div className={styles.card}>
+            <div className={styles.infoMain}>
+                <div className={styles.titleAvatar}>
+                    <h1 className={styles.title}>{name}</h1>
+                    <div className={styles.imgContainer}>
+                        <img src={avatar}/>
+                    </div>
+                </div>
+                <div className={styles.info}>
+                    {rate ? <div><span className={styles.infoBold}>Hourly rate: </span> £{rate}</div> : null}
+                    {region ? <div><span className={styles.infoBold}>Region:</span> {region}</div> : null}
+                    {base ? <div><span className={styles.infoBold}>Based out of: </span>{base}</div> : null}
+                    {radius ? <div><span className={styles.infoBold}>Radius covered: </span>{radius}</div> : null}
+                    {notes ? <div className={styles.areaNotes}><h4 className={styles.infoBold}>Area notes</h4>{notes}</div> : null}
+                </div>
+            </div>
+            <div className={styles.bio}>{bio}</div>
+            <Link className={styles.update} to='/portal/update'>Update profile</Link>
+        </div>
     </div>
 )
 
