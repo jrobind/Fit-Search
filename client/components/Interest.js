34d6438f40a664e3dd173eb0f5ec249feddfaee5
James Robinson <jrobind@outlook.com>
2018-04-10 13:28:27 +0100

    add Interest component styles

--- client/components/Interest.js
+++ client/components/Interest.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from '../styles/components/interest.css';
 
 const Interest = ({ requests, trainerId, handleRemoveInterest }) => (
-    <div>
-        {!requests.length ? <p>No requests found...</p> :
-            <div className="interest-container">  
+    <div className={styles.background}>
+        {!requests.length ? <p className={styles.noRequests}>No requests found...</p> :
+            <div className={styles.interestContainer}>  
                 {requests.map(({ requestee, _id }) => (
-                    <div key={_id} className="interest-card">
-                        <div>User email: {requestee.email}</div> 
-                        <div>User name: {requestee.profile.name}</div> 
-                        <img src={requestee.profile.avatar}/>
-                        <div>User bio: {requestee.profile.bio}</div>
-                        <div>Already contacted the user?</div>
-                        <button onClick={() => {
-                            handleRemoveInterest(_id);
-                        }}>Remove request</button>
+                    <div key={_id} className={styles.card}>
+                        <img className={styles.avatar} src={requestee.profile.avatar}/>
+                        <div className={styles.info}>
+                            <div><span className={styles.email}>User email: </span>{requestee.email}</div> 
+                            <div><span className={styles.username}>Username: </span>{requestee.profile.name}</div> 
+                        </div>
+                        <div className={styles.bioContainer}>
+                            <h4 className={styles.bio}>User bio</h4>
+                            <div>{requestee.profile.bio}</div>
+                        </div>
+                        <div className={styles.contactedContainer}>
+                            <div className={styles.contacted}>
+                                <span>Already contacted the user?</span>
+                                <button onClick={() => {
+                                    handleRemoveInterest(_id);
+                                }}>Remove request</button>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
