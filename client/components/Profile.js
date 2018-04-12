70ce086c34d01d6843261fc7d8a84aa2330219fa
James Robinson <jrobind@outlook.com>
2018-04-12 18:33:58 +0100

    wrap profile card in container and update Profile component styles

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -22,53 +22,55 @@ const Profile = ({
     interestRegistered
 }) => (
     <div className={styles.profileContainer}>
-    {locationState && locationState.profileUpdated ? <div className={styles.updateSuccess}>Profile updated!</div> : null}
-
-        <div className={styles.card}>
-            <div className={!rate ? styles.clientTop : styles.top}>
-                <div className={styles.avatar}>
-                    <div className={styles.imgContainer}>
-                        <img src={avatar}/>
+        {locationState && locationState.profileUpdated ? <div className={styles.updateSuccess}>Profile updated!</div> : null}
+    
+        <div className={styles.cardContainer}>
+            <div className={styles.card}>
+                <div className={!rate ? styles.clientTop : styles.top}>
+                    <div className={styles.avatar}>
+                        <div className={styles.imgContainer}>
+                            <img src={avatar}/>
+                        </div>
                     </div>
+
+                    <div className={styles.nameAndReview}>
+                        <h2>{name}</h2>
+                        {reviews.length ? <div className={styles.reviewInfo}>
+                            {handleReviewStars(reviewAverage)}
+                            <div>({reviews.length})</div>
+                        </div> : null}
+                    </div>
+
+                    {!rate ? null : <div className={styles.info}>
+                        <span>{base}</span> |
+                        <span>{region}</span> |
+                        <span>£{rate}</span> |
+                        <span>{radius} mile radius</span>
+                    </div>}
+
+                    {!notes ? null : <div className={styles.notes}>
+                        <h4>Area notes</h4>
+                        <div>{notes}</div>
+                    </div>}
+
+                    {typeof updateLink !== 'function' ? 
+                        <Link 
+                            className={!rate ? styles.clientUpdate : styles.update} to='/portal/update'
+                        >
+                            Update profile
+                        </Link> : 
+                        <button 
+                            className={interestRegistered ? styles.disable : styles.interest} 
+                            onClick={() => updateLink()} 
+                            disabled ={interestRegistered ? true : false}
+                        >
+                            {interestRegistered ? 'Interest registered' : 'Register interest'}
+                        </button>}
                 </div>
 
-                <div className={styles.nameAndReview}>
-                    <h2>{name}</h2>
-                    {reviews.length ? <div className={styles.reviewInfo}>
-                        {handleReviewStars(reviewAverage)}
-                        <div>({reviews.length})</div>
-                    </div> : null}
+                <div className={styles.bottom}> 
+                    <div className={styles.bio}>{bio}</div>
                 </div>
-
-                {!rate ? null : <div className={styles.info}>
-                    <span>{base}</span> |
-                    <span>{region}</span> |
-                    <span>£{rate}</span> |
-                    <span>{radius} mile radius</span>
-                </div>}
-                    
-                {!notes ? null : <div className={styles.notes}>
-                    <h4>Area notes</h4>
-                    <div>{notes}</div>
-                </div>}
-
-                {typeof updateLink !== 'function' ? 
-                    <Link 
-                        className={!rate ? styles.clientUpdate : styles.update} to='/portal/update'
-                    >
-                        Update profile
-                    </Link> : 
-                    <button 
-                        className={interestRegistered ? styles.disable : styles.interest} 
-                        onClick={() => updateLink()} 
-                        disabled ={interestRegistered ? true : false}
-                    >
-                        {interestRegistered ? 'Interest registered' : 'Register interest'}
-                    </button>}
-            </div>
-
-            <div className={styles.bottom}> 
-                <div className={styles.bio}>{bio}</div>
             </div>
         </div>
     </div>
