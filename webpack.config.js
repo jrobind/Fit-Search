318f574b8fc120baec7c72dad00a7d2807daae81
James Robinson <jrobind@outlook.com>
2018-04-15 14:38:51 +0100

    install babel-polyfill and update webpack.config for IE11 support

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry: './client/index.js',
+    entry: ['babel-polyfill', './client/index.js'],
     output: {
         path: '/',
         filename: 'bundle.js'
