ea19271c57dd0700342d9e81708f39349d644e9f
James Robinson <jrobind@outlook.com>
2018-04-19 19:12:28 +0100

    add 'no-cache' header to all api requests to prevent IE cache

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 
+// prevent IE caching
+const config = {
+    headers: {
+        Pragma: 'no-cache'
+    }
+}
+
 export const apiUpdateProfile = (id, data) => {
-    return axios.put('/api/profile', {id, data})
+    return axios.put('/api/profile', {id, data}, config)
         .then((data) => data)
         .catch((error) => console.log(error)); 
 }
 
 export const apiGetProfile = (id) => {
-    return axios.get(`/api/profile/find/${id}`)
+    return axios.get(`/api/profile/find/${id}`, config)
         .then((data) => data)
         .catch((error) => console.log(error)); 
 }
 
 export const apiRegisterUser = (data) => {
-    return axios.post('/api/register', data)
+    return axios.post('/api/register', data, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
@@ -23,43 +30,43 @@ export const apiCreateReview = (trainerId, review) => {
         trainerId,
         reviewData: review 
     };
-    return axios.post('/api/review', reviewData)
+    return axios.post('/api/review', reviewData, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }   
 
 export const apiLoginUser = (data) => {
-    return axios.post('/api/login', data)
+    return axios.post('/api/login', data, config)
         .then((data) => data)
         .catch((error) => error);
 }
 
 export const apiLogoutUser = () => {
-    return axios.get('/api/logout')
+    return axios.get('/api/logout', config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const apiGetSeachQuery = (query) => {
-    return axios.get(`/api/profile/trainers/${query}`)
+    return axios.get(`/api/profile/trainers/${query}`, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const apiCreateInterestRequest = (data) => {
-    return axios.post('/api/interest', data)
+    return axios.post('/api/interest', data, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const apiGetInterestRequests = (id) => {
-    return axios.get(`/api/interest/${id}`)
+    return axios.get(`/api/interest/${id}`, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const apiRemoveInterestRequest = (id) => {
-    return axios.delete(`/api/interest/${id}`)
+    return axios.delete(`/api/interest/${id}`, config)
         .then((data) => data)
         .catch((error) => console.log(error));
 }
